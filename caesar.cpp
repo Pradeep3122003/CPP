b731f4fafb4a7c7c24ac916b5e058065fda43cd6
Pradeep3122003 <pradeepkh312@gmail.com>
2025-01-28 14:24:22 +0530

    Caesar cipher first draft

--- caesar.cpp
+++ caesar.cpp
@@ -1,22 +1,96 @@
 #include <iostream>
 using namespace std;
 
+class caesar{
+public:
+  string input, output;
+  int key;
+
+  caesar(){
+   cout<<"Enter key: ";
+   cin>>key;
+   cout<<"\n---------------------------"<<endl;
+   keycheck();
+  }
+  
+  void keycheck()
+  { 
+    key=key%26;
+    string s=(key > 13)?"Great":"Good";
+    cout<<"Key: "<<key<<endl;
+    cout<<"Strength: "<<s<<endl;
+    cout<<"Continue/re-enter(y/n): ";
+    char a;
+    cin>>a;
+    if(a=='n')
+    {
+      caesar();
+    }
+    else
+    {
+     choice();
+    }
+
+  }
+
+  void choice()
+  {
+     int a;
+     while(1==1)
+     {
+     cout<<"1. Key  2. Encrypt  3. Decrypt"<<"\noption: ";
+     cin>>a;
+
+     switch(a)
+     {
+       case 1: keycheck();
+           break;
+       case 2: encrypt();
+            break;
+       case 3: decrypt();
+            break;
+       default: cout<<"\nWrong option"<<endl;
+            break;
+     }
+    }
+  }
+
+void encrypt()
+{
+  input="";
+  output="";
+  cout<<"Message: ";
+  cin>>input;
+  for(int i=0;i<input.size();i++)
+  {
+   output+=input[i]+key;
+  }
+  cout<<"\nCipher: "<<output;
+  
+}
+
+void decrypt()
+{
+  input="";
+  output="";
+  cout<<"Cipher: ";
+  cin>>input;
+  for(int i=0;i<input.size();i++)
+  {
+   output+=input[i]-key;
+  }
+  cout<<"\nDecrypt: "<<output;
+  
+}
+
+};
+
 
 
 int main()
 {
 
- string a;
- cin>>a;
- for(int i=0;i<a.size();i++)
-{
-  if (a[i] >90)
-  {
-    a[i]-=32;
-    cout<<(int) a[i];
-  }
-}
-cout<<a;
+caesar obj1;
 
 return 0;
 }
